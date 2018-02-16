57c1ae55edc30276882e8cc8f594b1c9bb10776a
lifecoder <lifecoder.ua@gmail.com>
2018-02-17 00:50:00 +0200

    better layout on patients

--- src/components/Patient.vue
+++ src/components/Patient.vue
@@ -1,22 +1,24 @@
 <template>
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <v-list two-line>
-          <template v-for="(item, index) in items">
-            <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
-            <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
-            <v-list-tile avatar v-else :key="item.title">
-              <v-list-tile-avatar>
-                <img :src="item.avatar">
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </template>
-        </v-list>
+      <v-layout row>
+        <v-flex>
+          <v-list two-line>
+            <template v-for="(item, index) in items">
+              <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+              <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+              <v-list-tile avatar v-else :key="item.title">
+                <v-list-tile-avatar>
+                  <img :src="item.avatar">
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                  <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </template>
+          </v-list>
+        </v-flex>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
